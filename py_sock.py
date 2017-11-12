92ed0a582d16a6742dc032fb0ce3c0cff311f9fc
vastopol <classicunix@gmail.com>
2017-11-11 20:01:54 -0800

    condense stuff

--- py_sock.py
+++ py_sock.py
@@ -15,28 +15,44 @@ notes = ['C4', 'C#4', 'D4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4']  
 synth = types[1] # current synth (user can change)
 length = .1      # time interval for notes (user can change)
 amount = 1       # number of times selected part is played (user can change)
+dictate = {}     # dictionary to save notes
 #-------------------------------
 
 def main(argv):
-    global types,notes,synth,length,amount
+    global types,notes,synth,length,amount,dictate
     while True:
         listy = getargs()
         for i in listy:
             #print i[0]
-            if i[0] == 'play':
+            if i[0] == 'save':
+                i.pop(0)
+                if len(i) < 2:
+                    print("ERR save format")
+                else:
+                    bop = i.pop(0)
+                    dictate[bop] = i
+            elif i[0] == 'scale':
                 if len(i) == 1:
-                    sox_play_scale(None) # default
+                    sox_play(None,0) # default
+                elif len(i) == 2:
+                    i.pop(0)
+                    temp = map(str.upper,dictate[i[0]])
+                    sox_play(temp,0) # default
                 else:
                     i.pop(0)
                     i = map(str.upper,i)
-                    sox_play_scale(i)
+                    sox_play(i,0)
             elif i[0] == 'chord':
                 if len(i) == 1:
-                    sox_play_chord(None) # default
+                    sox_play(None,1) # default
+                elif len(i) == 2:
+                    i.pop(0)
+                    temp = map(str.upper,dictate[i[0]])
+                    sox_play(temp,1) # default
                 else:
                     i.pop(0)
                     i = map(str.upper,i)
-                    sox_play_chord(i)
+                    sox_play(i,1)
             elif i[0] == 'time' and len(i) >1:
                 if float(i[1]) >= 0:
                     length = float(i[1])
@@ -54,6 +70,11 @@ def main(argv):
                     print("ERR unknown synth: " + i[1])
             elif i[0] == 'show':
                 show()
+            elif i[0] == 'reset':
+                synth = types[1]
+                length = .1
+                amount = 1
+                dictate = {}
             elif i[0] == 'stop':
                 silence()
             elif i[0] == 'quit':
@@ -80,6 +101,10 @@ def show():
     print("synthesizer = " + synth)
     print("time length = " + str(length))
     print("repeat amount = " + str(amount))
+    if len(dictate) > 0:
+        print("saved items")
+        for i in dictate:
+            print(i + ' ' + str(dictate[i]))
 #---------------------------------
 
 def silence():
@@ -93,41 +118,15 @@ def quit():
 
 # Wrapper
 # var either None or list
-def sox_play_scale(var):
-    if var != None:
-        for j in range(amount):
-            for i in range(len(var)):
-                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
-                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + var[i] # str to be formated and filled in
-                print(var[i])
-                player(s,0)
-    else:
-        for j in range(amount):
-            for i in range(len(notes)):
-                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
-                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + notes[i] # str to be formated and filled in
-                print(notes[i])
-                player(s,0)
-#------------------------------
-
-
-# Wrapper
-# var either None or list
-def sox_play_chord(var):
-    if var != None:
-        for j in range(amount):
-            for i in range(len(var)):
-                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
-                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + var[i] # str to be formated and filled in
-                print(var[i])
-                player(s,1)
-    else:
-        for j in range(amount):
-            for i in range(len(notes)):
-                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
-                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + notes[i] # str to be formated and filled in
-                print(notes[i])
-                player(s,1)
+# choice 0 = scale, 1 = chord
+def sox_play(var,choice):
+    if var == None:
+        var = notes
+    for j in range(amount):
+        for i in range(len(var)):
+            s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + var[i] # str to be formated and filled in
+            print(var[i])
+            player(s,choice)
 #------------------------------
 
 # 0 = scale, 1 = chord
