272d8c3d73c5b7b49674d65bbbf9d227eb078ce2
vastopol <classicunix@gmail.com>
2017-11-11 18:55:59 -0800

    basic version

--- README.md
+++ README.md
@@ -3,21 +3,12 @@ command line synthesizer project
 
 * dependencies
   * sox - cross-platform audio editing software (version 14.4.2)
-  * pysox - Python wrapper around sox (version 1.3.0)
 
 To install SoX on Linux:
 apt-get install sox
 
-To install the most up to date release of python module via PyPi:
-pip install sox
-
 this project uses python 2.7 for now.
-for some reason python3 doesn't work properly "ImportError: No module named 'sox'"
 
 http://sox.sourceforge.net/sox.html
 
 https://linux.die.net/man/1/sox
-
-https://github.com/rabitt/pysox
-
-http://pysox.readthedocs.io/en/latest/
--- py_sock.py
+++ py_sock.py
@@ -6,13 +6,14 @@ import sys
 import subprocess
 import time
 import os
+import itertools
 #-------------------------------
 
 # Global Variables
 types = ['', 'pluck', 'sine', 'square', 'triangle', 'sawtooth', 'trapezium', 'exp', 'noise'] # synth types, 0 = empty where default is sine
-notes = ['C4', 'C#4', 'D4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4']      # chromatic scale in c
+notes = ['C4', 'C#4', 'D4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4']      # chromatic scale in c (default)
 synth = types[1] # current synth (user can change)
-length = .2      # time interval for notes (user can change)
+length = .1      # time interval for notes (user can change)
 amount = 1       # number of times selected part is played (user can change)
 #-------------------------------
 
@@ -21,9 +22,21 @@ def main(argv):
     while True:
         listy = getargs()
         for i in listy:
-            print i[0]
+            #print i[0]
             if i[0] == 'play':
-                sox_play()
+                if len(i) == 1:
+                    sox_play_scale(None) # default
+                else:
+                    i.pop(0)
+                    i = map(str.upper,i)
+                    sox_play_scale(i)
+            elif i[0] == 'chord':
+                if len(i) == 1:
+                    sox_play_chord(None) # default
+                else:
+                    i.pop(0)
+                    i = map(str.upper,i)
+                    sox_play_chord(i)
             elif i[0] == 'time' and len(i) >1:
                 if float(i[1]) >= 0:
                     length = float(i[1])
@@ -62,26 +75,6 @@ def getargs():
     return line4
 #-------------------------------
 
-def sox_play():
-    for j in range(amount):
-        for i in range(len(notes)):
-            # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
-            s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + notes[i] # str to be formated and filled in
-            print(notes[i])
-            player(s)
-#------------------------------
-
-def player(sound):
-    # OPTION 1
-    # sox.core.play(sound) # doesnt work...
-    # OPTION 2
-    # proc = subprocess.Popen(sound,shell=True)
-    # time.sleep(delays)
-    # subprocess.call('pkill play',shell=True)
-    # OPTION 3
-    os.system(sound)
-#---------------------------------
-
 def show():
     print("current settings")
     print("synthesizer = " + synth)
@@ -94,9 +87,63 @@ def silence():
 #---------------------------------
 
 def quit():
+    subprocess.call('killall play',shell=True)
     sys.exit(0)
+#---------------------------------
+
+# Wrapper
+# var either None or list
+def sox_play_scale(var):
+    if var != None:
+        for j in range(amount):
+            for i in range(len(var)):
+                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
+                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + var[i] # str to be formated and filled in
+                print(var[i])
+                player(s,0)
+    else:
+        for j in range(amount):
+            for i in range(len(notes)):
+                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
+                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + notes[i] # str to be formated and filled in
+                print(notes[i])
+                player(s,0)
+#------------------------------
+
+
+# Wrapper
+# var either None or list
+def sox_play_chord(var):
+    if var != None:
+        for j in range(amount):
+            for i in range(len(var)):
+                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
+                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + var[i] # str to be formated and filled in
+                print(var[i])
+                player(s,1)
+    else:
+        for j in range(amount):
+            for i in range(len(notes)):
+                # s = ['play -qn synth', str(length), synth, notes[i]] # str to be formated and filled in
+                s = 'play -qn synth' + ' ' + str(length) + ' ' + synth + ' ' + notes[i] # str to be formated and filled in
+                print(notes[i])
+                player(s,1)
+#------------------------------
+
+# 0 = scale, 1 = chord
+def player(sound,option=0):
+    # OPTION 1
+    # sox.core.play(sound) # doesnt work...
+    # OPTION 2
+    # proc = subprocess.Popen(sound,shell=True)
+    # time.sleep(delays)
+    # subprocess.call('pkill play',shell=True)
+    # OPTION 3
+    if option == 0:
+        os.system(sound)
+    elif option == 1:
+        os.system(sound + ' ' + '&')
 #---------------------------------
 
 if __name__ == '__main__':
     main(sys.argv)
-#-------------------------------
